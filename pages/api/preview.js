d88ea18b963b1e1afa12b8cc94d9259e59ace3aa
mathiasdag <mathias@asso.gd>
2021-03-30 11:54:35 +0200

    ...

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -3,7 +3,7 @@ import { getPreviewPostBySlug } from '../../lib/api'
 export default async function preview(req, res) {
   const { secret, slug } = req.query
 
-  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
+  if (secret !== process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_SECRET || !slug) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
